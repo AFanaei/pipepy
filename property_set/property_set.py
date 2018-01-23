b196ae53fa4e3919d110f2e075aa9910f5e684b5
AhmadF <fanaei.ahmad@gmail.com>
2018-01-23 16:18:17 +0330

    change tests to use new propertySet component

--- property_set/property_set.py
+++ property_set/property_set.py
@@ -3,7 +3,7 @@ from scipy import constants, math
 from .comp_factor import CompFactorInterpolator
 
 
-class cached_property:
+class cached_property(object):
     def __init__(self, func, name=None):
         self.func = func
         self.__doc__ = getattr(func, '__doc__')
@@ -15,23 +15,21 @@ class cached_property:
         res = instance.__dict__[self.name] = self.func(instance)
         return res
 
-    @staticmethod
-    def invalidate_cache(instance):
-        for property_name in dir(instance):
-            if isinstance(getattr(instance, property_name), cached_property):
-                instance.__dict__.pop(property_name, None)
 
-
-class PropertySet:
+class PropertySet(object):
     def __init__(self, z_calculator=None):
         self.z_calculator = z_calculator if z_calculator is not None else CompFactorInterpolator()
-        self.P = 1   # unit is pa
-        self.T = 0   # unit is kelvin
-        self.M = 16  # unit is g/mol
+        self.P = None   # unit is pa
+        self.T = None   # unit is kelvin
+        self.MW = None  # unit is g/mol
 
-    def __set__(self, instance, value):
-        self.__dict__[instance] = value
-        cached_property.invalidate_cache(self)
+    def invalidate_cache(self):
+        for property_name in dir(self):
+            try:
+                if isinstance(getattr(type(self), property_name), cached_property):
+                    self.__dict__.pop(property_name, None)
+            except:
+                pass
 
     @cached_property
     def Z(self):
@@ -54,8 +52,12 @@ class PropertySet:
         return 2314
 
     @cached_property
+    def ro(self):
+        return self.P * self.MW / (self.Z * constants.R * self.T)
+
+    @cached_property
     def v_w(self):
         return math.sqrt(
-            self.Z * constants.R * self.T / self.M /
+            self.Z * constants.R * self.T / self.MW /
             (1 - self.P / self.Z * self.dz_dp - self.P / (self.ro * self.c_p * self.T) * (1 + self.T / self.Z * self.dz_dt)**2)
         )
--- test/propertySetTest.py
+++ test/propertySetTest.py
@@ -5,12 +5,13 @@ import unittest
 
 import numpy as np
 
-from property_set.comp_factor import CompFactorInterpolator
+from property_set.property_set import PropertySet
 
 
 class ZTest(unittest.TestCase):
     def setUp(self):
-        self.z = CompFactorInterpolator()
+        self.ps = PropertySet()
+        self.ps.MW = 16.04 / 1000
         address = os.path.join(
             os.path.dirname(__file__), os.pardir, 'databases', "z.json")
 
@@ -27,17 +28,21 @@ class ZTest(unittest.TestCase):
 
     def testZ(self):
         for i in self.points:
-            res = self.z.get_z(i[0], i[1])
-            self.assertLessEqual(abs(res - i[2]), i[2] * 0.001, "diffrence in result")
+            self.ps.P = i[0] * 10**5
+            self.ps.T = i[1] + 273.15
+            self.ps.invalidate_cache()
+            self.assertLessEqual(abs(self.ps.Z - i[2]), i[2] * 0.001)
 
     def testZInterp(self):
         for i in self.interp:
             p = [np.average([i[0][j], i[1][j]]) for j in range(3)]
-            res = self.z.get_z(p[0], p[1])
+            self.ps.P = p[0] * 10**5
+            self.ps.T = p[1] + 273.15
+            self.ps.invalidate_cache()
             # we cant check exactly just check the interpolated value is between the two bounds.
             # check to make shure calculated z is between the two points.
-            one = i[0][2] - res
-            two = res - i[1][2]
+            one = i[0][2] - self.ps.Z
+            two = self.ps.Z - i[1][2]
             self.assertGreater(one * two, 0,
                                "z:{},P:{},T:{} not between {k[0][0]}-{k[1][0]},{k[0][1]}-{k[1][1]}"
-                               .format(res, p[0], p[1], k=i))
+                               .format(self.ps.Z, p[0], p[1], k=i))
